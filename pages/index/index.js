3aac552074a1e7107ea5c3579a5178e9f2603c19
周后云 <yizzuide@ZhouHouYundeiMac.local>
2017-07-24 17:24:01 +0800

    “”

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,15 +3,35 @@
 var app = getApp()
 Page({
   data: {
+    num:'',
     motto: 'Hello World',
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  listenerLogin: function() {
+    if(this.data.num.length == 0){
+      wx.showModal({
+        title: '温馨提示',
+        content: '请填写卡号',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+    }else {
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
+    }
+  },
+
+  listenerPhoneInput: function (e) {
+    this.setData({
+      num: e.detail.value
     })
   },
+
   onLoad: function () {
     console.log('onLoad')
     var that = this
@@ -24,3 +44,24 @@ Page({
     })
   }
 })
+
+// var rootDocment = 'hxxxxx';//你的域名  
+// function req(url, data, cb) {
+//   wx.request({
+//     url: rootDocment + url,
+//     data: data,
+//     method: 'post',
+//     header: { 'Content-Type': 'application/json' },
+//     success: function (res) {
+//       return typeof cb == "function" && cb(res.data)
+//     },
+//     fail: function () {
+//       return typeof cb == "function" && cb(false)
+//     }
+//   })
+// }
+
+
+// module.exports = {
+//   req: req
+// }  
